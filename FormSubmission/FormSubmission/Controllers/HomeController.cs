23266811c6700edca689d1735c6539dcbe8ddad3
Rnugg <jordans11409@yahoo.com>
2020-01-20 14:16:43 -0800

    Add files via upload

---
+++ FormSubmission/FormSubmission/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FormSubmission.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FormSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(User response)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Success");
+            }
+
+            return View();
+        }
+    }
+}
